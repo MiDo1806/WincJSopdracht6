aff747fd03e0be6f982b6f8dc7c08fc96279e962
MiDo1806 <81757415+MiDo1806@users.noreply.github.com>
2021-12-05 11:45:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const makePizza = function () {
+    console.log('Make dough');
+    console.log('Make pastasauce');
+    console.log('Bake dough');
+    console.log('Add pasta on dough');
+    console.log('Add more ingredients to your liking');
+    console.log('Bake pizza in the oven');
+}
+makePizza();
+makePizza();
+const makeSushi = function () {
+    console.log('Cut all ingredients')
+    console.log('Add ingredients to roll them up')
+    console.log('Add sauce')
+    console.log('Try to eat with sticks')
+}
+makeSushi();
+makeSushi();
+makeSushi();
